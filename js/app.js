d8d185dbb39412cb43bb7d8ddd1c08195817187d
Your Name <you@example.com>
2018-09-27 15:29:47 -0400

    update readme and code

--- js/app.js
+++ js/app.js
@@ -3,61 +3,60 @@ let game = true;
 
 // Enemies our player must avoid
 const Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.height = 65;
-    this.width = 95;
-    this.collision = false;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.height = 65;
+  this.width = 95;
+  this.collision = false;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for gameOver
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the gameOver runs at the same speed for
-    // all computers.
-    // this.x += 180 * dt;
-    if (this.x > ctx.canvas.width + this.width) {
-      this.x = -200 * Math.floor(Math.random() * 4) + 1;
+  // You should multiply any movement by the dt parameter
+  // which will ensure the gameOver runs at the same speed for
+  // all computers.
+  // this.x += 180 * dt;
+  if (this.x > ctx.canvas.width + this.width) {
+    this.x = -200 * Math.floor(Math.random() * 4) + 1;
+  } else {
+    this.x += 250 * dt;
+  }
+
+  // collision with Player
+  // // proximity
+  if (collision(player.x, player.y, player.width, player.height, this.x, this.y, this.width, this.height)) {
+    this.collision = true;
+
+    if (player) {
+      player.x = 204;
+      player.y = 400;
+      alert('Boom!');
+
     } else {
-        this.x += 250 * dt;
+      this.collision = false;
     }
-
-    // collision with Player
-    // // proximity
-    if (collision(player.x, player.y, player.width, player.height, this.x, this.y, this.width, this.height)){
-         this.collision = true;
-
-       if(player){
-            player.x = 204;
-            player.y = 400;
-             alert('Boom!');
-
-     } else {
-        this.collision = false;
-     }
   }
 };
 
 function collision(px, py, pw, ph, ex, ey, ew, eh) {
-    return (Math.abs(px - ex) * 2 < pw + ew) && (Math.abs(py - ey) * 2 < ph + eh);
+  return (Math.abs(px - ex) * 2 < pw + ew) && (Math.abs(py - ey) * 2 < ph + eh);
 }
 // Draw the enemy on the screen, required method for gameOver
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-class Player {
-  constructor(x, y, sprite) {
+const Player = function(x, y, sprite) {
     // x and y position of player
     this.x = x;
     this.y = y;
@@ -65,44 +64,35 @@ class Player {
     this.sprite = sprite;
     this.height = 75;
     this.width = 65;
-  }
-}
-// const Player = function(x, y, sprite) {
-//     // x and y position of player
-//     this.x = x;
-//     this.y = y;
-//     this.sprite = sprite;
-//     this.height = 75;
-//     this.width = 65;
-//   };
+  };
 
 Player.prototype.update = function(dt) {
   for (let enemy of allEnemies) {
-    if (game && player.y <= 40 && game && enemy.y >= 40) {
-       game = false;
-       youWin();
-       }
-   }
+    if (game && this.y <= 40 && game && enemy.y >= 40) {
+      game = false;
+      youWin();
+    }
+  }
 
 }
- // Draw the player sprite on the screen
+// Draw the player sprite on the screen
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(direction) {
   const horizontal = 101;
   const vertical = 83;
 
-    if (direction === 'left' && this.x - horizontal >= 0 ) {
-        this.x -= horizontal;
-    } else if (direction === 'right' && this.x + horizontal < ctx.canvas.width ) {
-        this.x += horizontal;
-    } else if (direction === 'up' && this.y - vertical > 0 - player.height) {
-        this.y -= vertical;
-    } else if (direction === 'down' && this.y + vertical < ctx.canvas.height - 200 ) {
-        this.y += vertical;
-    }
+  if (direction === 'left' && this.x - horizontal >= 0) {
+    this.x -= horizontal;
+  } else if (direction === 'right' && this.x + horizontal < ctx.canvas.width) {
+    this.x += horizontal;
+  } else if (direction === 'up' && this.y - vertical > 0 - player.height) {
+    this.y -= vertical;
+  } else if (direction === 'down' && this.y + vertical < ctx.canvas.height - 200) {
+    this.y += vertical;
+  }
 }
 
 
@@ -113,14 +103,14 @@ Player.prototype.handleInput = function(direction) {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    let allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  let allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
 // function collision(px, py, pw, ph, ex, ey, ew, eh) {
@@ -130,17 +120,17 @@ document.addEventListener('keyup', function(e) {
 // play win
 function youWin() {
   let name = prompt('What is your name?');
-    // player.x = 204;
-    // player.y = 400;
-    alert('Congrats, ' + name + ' you Won!');
-    reset();
+  // player.x = 204;
+  // player.y = 400;
+  alert('Congrats, ' + name + ' you Won!');
+  reset();
 };
 
 // to clear enemies from board
 function reset() {
-    allEnemies = [];
-    // player.x = 0;
-    // player.y = 0;
+  allEnemies = [];
+  // player.x = 0;
+  // player.y = 0;
 };
 
 // Variables
@@ -150,7 +140,7 @@ const enemyLocations = [60, 145, 227];
 // const bug3 = new Enemy();
 
 let allEnemies = enemyLocations.map((y, index) => {
-  return new Enemy(( -150 * (index + 1)), y)
+  return new Enemy((-150 * (index + 1)), y)
 });
 
 let player = new Player(204, 400, 'images/char-pink-girl.png');
